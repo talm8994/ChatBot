3fe1fb6b00bef8b6a5ea15c4640d45870053af36
talm8994 <tom.tom.pop13@gmail.com>
2015-11-05 13:44:15 -0700

    new icons

--- src/chat/view/ChatPopupView.java
+++ src/chat/view/ChatPopupView.java
@@ -1,6 +1,8 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+
 /**
  * pops up GUI.
  * @author talm8994
@@ -8,16 +10,55 @@ import javax.swing.JOptionPane;
  */
 public class ChatPopupView
 {
+	private String windowMessage;
+	private ImageIcon chatIcon;
+	private ImageIcon sansIcon;
+	private ImageIcon papyIcon;
+	private ImageIcon pearlIcon;
+	private ImageIcon jakeIcon;
+	public ChatPopupView()
+	{
+		windowMessage = ("this is chatbot");
+		papyIcon = new ImageIcon(getClass().getResource("images/papyras.jpg"));
+		chatIcon = new ImageIcon(getClass().getResource("images/megaman_icon.png"));
+		sansIcon = new ImageIcon(getClass().getResource("images/sans.png"));
+		pearlIcon = new ImageIcon(getClass().getResource("images/pearl.jpeg"));
+		jakeIcon = new ImageIcon(getClass().getResource("images/jake.png"));
+	}
+	
 	public String getAnswer(String displayMessage)
 	{
 		String answer = "";
 		
-		answer = JOptionPane.showInputDialog(null, displayMessage);
+		answer = JOptionPane.showInputDialog(null, displayMessage, windowMessage, JOptionPane.INFORMATION_MESSAGE, RandomIcon(), null, "Type here please!").toString();
 				
 	    return answer;
 	}
 	public void display(String displayMessage)
 	{
-		JOptionPane.showMessageDialog(null, displayMessage);
+		JOptionPane.showMessageDialog(null, displayMessage, windowMessage, JOptionPane.PLAIN_MESSAGE, RandomIcon());
+	}
+	
+	private ImageIcon RandomIcon()
+	{
+		int RandomIcon = (int) (Math.random() * 5);
+		
+		switch(RandomIcon)
+		{
+		case 0:
+			return papyIcon;
+		case 1:
+			return sansIcon;
+		case 2: 
+			return chatIcon;
+		case 3:
+			return pearlIcon;
+		case 4:
+			return jakeIcon;
+		default:
+			return chatIcon;
+			
+		}
+		
 	}
 }
