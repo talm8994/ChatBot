98993bce59242b44ed95f53acc3d4707c6397934
talm8994 <tom.tom.pop13@gmail.com>
2015-11-05 12:31:30 -0700

    constructor

--- src/chat/view/ChatPopupView.java
+++ src/chat/view/ChatPopupView.java
@@ -1,6 +1,7 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 /**
  * pops up GUI.
  * @author talm8994
@@ -8,6 +9,15 @@ import javax.swing.JOptionPane;
  */
 public class ChatPopupView
 {
+	private String windowMessage;
+	private ImageIcon chatIcon;
+	
+	public ChatPopupView()
+	{
+		windowMessage = ("this is chatbot");
+		chatIcon = new ImageIcon(getClass().getResource("images/chatbot.png"));
+	}
+	
 	public String getAnswer(String displayMessage)
 	{
 		String answer = "";
