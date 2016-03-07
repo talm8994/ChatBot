addd8b174e838dd4c689e3ea086d8ff2db361a3b
talm8994 <tom.tom.pop13@gmail.com>
2016-03-07 12:41:48 -0700

    twitter

---
+++ src/chat/model/CTECTwitter.java
@@ -0,0 +1,22 @@
+package chat.model;
+
+import java.util.*;
+import twitter4j.*;
+
+public class CTECTwitter
+{
+		private ArrayList <Status> statusList;
+		private ArrayList <String> wordsList;
+		
+		public CTECTwitter()
+		{
+			statusList = new ArrayList<Status>();
+			wordsList = new ArrayList<String>();
+		}
+		
+		public void sendTweet(String message)
+		{
+			
+		}
+
+}
